f925cbbb1f65ad590757fd240131d5d311b06eab
Roman <romanahramenya@gmail.com>
2022-05-18 03:09:52 +0300

    second commit

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: 'AIzaSyDm4c8FVtQ_qX2a8P5nIitu9CLk5EMeo3s',
+  authDomain: 'auth-69b7f.firebaseapp.com',
+  projectId: 'auth-69b7f',
+  storageBucket: 'auth-69b7f.appspot.com',
+  messagingSenderId: '423105794115',
+  appId: '1:423105794115:web:f354e24e233de79e3ac4f6'
 };
 
 // Initialize Firebase
